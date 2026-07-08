ee9b1269725d58efa6c9b86c9a64ea6cf85f7fec
Altaf Shaikh <altafmohdshaikh@gmail.com>
2026-07-08 20:09:32 +0530

    Add files via upload

---
+++ Sql/2ndbadge.sql
@@ -0,0 +1,37 @@
+-- Query a list of CITY and STATE from the STATION table.
+SELECT 
+CITY,
+STATE 
+FROM STATION;
+
+-- Query a list of CITY names from STATION for cities that have an even ID number. Print the results in any order, but exclude duplicates from the answer.
+SELECT DISTINCT CITY
+FROM STATION
+WHERE MOD(ID, 2) = 0;
+
+-- Find the difference between the total number of CITY entries in the table and the number of distinct CITY entries in the table.
+SELECT 
+COUNT(CITY) - COUNT(DISTINCT CITY)
+FROM STATION;
+
+-- Query the two cities in STATION with the shortest and longest CITY names, as well as their respective lengths (i.e.: number of characters in the name). If there is more than one smallest or largest city, choose the one that comes first when ordered alphabetically
+SELECT CITY,
+LENGTH(CITY)
+FROM STATION 
+WHERE LENGTH(CITY) = (
+    SELECT MAX(LENGTH(CITY)) 
+    FROM STATION
+)
+ORDER BY CITY;   
+
+SELECT CITY,
+LENGTH(CITY)
+FROM STATION 
+WHERE LENGTH(CITY) = (
+    SELECT MIN(LENGTH(CITY)) 
+    FROM STATION
+)
+ORDER BY CITY
+LIMIT 1;  
+
+-- 
