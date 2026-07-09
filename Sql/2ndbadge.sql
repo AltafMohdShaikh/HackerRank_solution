8543b42630c739eb7ebf1cead9793948e6183938
Altaf Shaikh <altafmohdshaikh@gmail.com>
2026-07-09 21:36:29 +0530

    Refactor SQL queries in 2ndbadge.sql

--- Sql/2ndbadge.sql
+++ Sql/2ndbadge.sql
@@ -34,4 +34,9 @@ WHERE LENGTH(CITY) = (
 ORDER BY CITY
 LIMIT 1;  
 
--- 
+-- Query the list of CITY names starting with vowels (i.e., a, e, i, o, or u) from STATION. Your result cannot contain duplicates.
+SELECT CITY
+FROM STATION
+WHERE LOWER(LEFT(CITY, 1)) IN ('a', 'e', 'i', 'o', 'u');
+
+--
