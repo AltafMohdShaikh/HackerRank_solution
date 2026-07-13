a6bf75ddcfafa0bd39d997b4fa980b2ddeb06b63
Altaf Shaikh <altafmohdshaikh@gmail.com>
2026-07-13 20:45:07 +0530

    Add query for CITY names ending with vowels
    Added a query to select distinct CITY names ending with vowels from the STATION table.

--- Sql/2ndbadge.sql
+++ Sql/2ndbadge.sql
@@ -39,4 +39,9 @@ SELECT CITY
 FROM STATION
 WHERE LOWER(LEFT(CITY, 1)) IN ('a', 'e', 'i', 'o', 'u');
 
---
+-- Query the list of CITY names ending with vowels (a, e, i, o, u) from STATION. Your result cannot contain duplicates.
+SELECT DISTINCT CITY
+FROM STATION
+WHERE LOWER(RIGHT(CITY, 1)) IN ('a', 'e', 'i', 'o', 'u');
+
+-- 
