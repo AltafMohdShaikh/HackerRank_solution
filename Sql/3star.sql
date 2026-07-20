55ab4510a2dc1b23b712ced0cf28a939bb78bf09
Altaf Shaikh <altafmohdshaikh@gmail.com>
2026-07-20 20:34:05 +0530

    Add query for students scoring above a threshold
    Add a query to select students with scores above a threshold, ordered by the last three characters of their names and then by ID.

--- Sql/3star.sql
+++ Sql/3star.sql
@@ -12,4 +12,4 @@ WHERE LOWER(LEFT(CITY, 1)) NOT IN ('a', 'e', 'i', 'o', 'u')
 AND
 LOWER(RIGHT(CITY, 1)) NOT IN ('a', 'e', 'i', 'o', 'u');
 
--- 
+-- Query the Name of any student in STUDENTS who scored higher than  Marks. Order your output by the last three characters of each name. If two or more students both have names ending in the same last three characters (i.e.: Bobby, Robby, etc.), secondary sort them by ascending ID.
